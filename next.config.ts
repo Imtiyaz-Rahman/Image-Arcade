c76f745643ff7445671d8fc9ebd28f03de954333
Imtiyaz Rahman <Imtiyaz@Rahman.ibm.com>
2025-01-15 15:10:52 +0000

    Added github pages and has been fully setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Image-Arcade/' : '',
+  basePath: isProd ? '/Image-Arcade' : '',
+  output: 'export'
   /* config options here */
 };
 
